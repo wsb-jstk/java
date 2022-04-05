8258acf4894610d28e34c78a92c7d5018dac6daf
Maciej Lipski <maciej.lipski@capgemini.com>
2022-04-05 19:39:58 +0200

    Optional with isPresent() & get()

--- src/test/java/com/campgemini/sample/OptionalTest.java
+++ src/test/java/com/campgemini/sample/OptionalTest.java
@@ -1,6 +1,8 @@
 package com.campgemini.sample;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -43,6 +45,35 @@ class OptionalTest {
         // then
         assertFalse(person.isPresent());
         assertTrue(person.isEmpty());
+    }
+
+    @Test
+    void shouldGetFullName_forPersonWithId1() {
+        // given
+        final int id = 1;
+        final Optional<Person> person = database.find(id);
+        // when
+        final String fullName = getFullName(person);
+        // then
+        assertEquals("Jan Kowalski", fullName);
+    }
+
+    @Test
+    void shouldThrowException_whenGettingFullName_forPersonWithId2() {
+        // given
+        final int id = 2;
+        final Optional<Person> person = database.find(id);
+        // when
+        assertThrows(RuntimeException.class, () -> getFullName(person));
+        // then
+    }
+
+    private String getFullName(Optional<Person> optionalPerson) {
+        if (optionalPerson.isPresent()) {
+            final Person person = optionalPerson.get();
+            return person.getFirstName() + " " + person.getLastName();
+        }
+        throw new RuntimeException("Empty person");
     }
 
 }
